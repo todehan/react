e8fd63e4f168a66d3bf6fd0281400c49891cfa12
todehan <todehan@gmail.com>
2019-04-29 11:13:14 +0300

    react basics

---
+++ array-filter.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4,5];
+
+const newNumbers1 = numbers.filter(number => number === 3);
+
+console.log(newNumbers1);
+
+const newNumbers2 = numbers.filter(number => number < 4);
+
+console.log(newNumbers2);
---
+++ object-asing.js
@@ -0,0 +1,6 @@
+const data1 = { name: "deneme1"};
+const data2 = { surname: "deneme2"};
+
+const result = Object.assign(data1, data2);
+
+console.log(result);
